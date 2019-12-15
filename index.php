d3cc434837f0361f4289fbb0f4ae264bc9e78375
Krfisc88 <krfisc@outlook.com>
2019-12-15 00:44:15 -0600

    touched up footer

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php include "templates/header.html"; ?>
 <?php include "templates/navbar.html"; ?>
 
-<div class="carousel">
-
-</div>
-<main>
+<main id="home-main">
   <h2>Welcome to Sky City Pizza!</h2>
-
+  <article>
+    <img id="pizza" src="../assets/pizza1.jpg" alt="delicious pizza" >
     <p>You've just landed where our drones take off carrying the delicious pizza
     made with 100% Organic & locally sourced ingredients, baked in our
     Aeronautical Radiant Heat Boiler Oven. Ok, actually we just use a regular
@@ -20,8 +18,6 @@
     velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
     cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
     est laborum.</p>
-
-  <img src="assets/pizza1.jpg" alt="delicious pizza" width="200">
-  <figcaption>Eat it clad in pinstripe and brass or don't, it's your life.</figcaption>
+  </article>
 </main>
 <?php include "templates/footer.html"; ?>
